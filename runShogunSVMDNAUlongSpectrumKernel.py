0a8ecc83bf092828c982cda81d24cc739c717774
Irene Kaplow <imk1@doublehelix.Stanford.EDU>
2015-08-21 04:48:03 -0700

    runShogunSVMDNAUlongSpectrumKernel.py now also has an option of reporting the performance of the majority class.

--- runShogunSVMDNAUlongSpectrumKernel.py
+++ runShogunSVMDNAUlongSpectrumKernel.py
@@ -69,9 +69,9 @@ def makeIntList(intFileName, skippedLines):
 	return np.array(intList)
 	
 	
-def runShogunSVMDNASpectrumKernel(train_xt, train_lt, test_xt):
+def runShogunSVMDNAUlongSpectrumKernel(train_xt, train_lt, test_xt):
 	"""
-	run svm with spectrum kernel
+	run svm with ulong spectrum kernel
 	"""
 
     ##################################################
@@ -166,11 +166,76 @@ def outputResultsClassification(out1, out2, out1DecisionValues, out2DecisionValu
 	print evaluatorValid.get_auROC()
 
 
+def outputResultsClassificationWithMajorityClass(out1, out2, out1DecisionValues, out2DecisionValues, train_lt, test_lt, test_majorityClass):
+	# Output the results to the appropriate output files
+	writeFloatList(out1, TRAINPREDICTIONSEPSILONFILENAME)
+	writeFloatList(out2, VALIDATIONPREDICTIONSEPSILONFILENAME)
+	
+	numTrainCorrect = 0
+	for i in range(len(train_lt)):
+		# Iterate through training labels and count the number that are the same as the predicted labels
+		if out1[i] == train_lt[i]:
+			# The current prediction is correct
+			numTrainCorrect = numTrainCorrect + 1
+	fracTrainCorrect = float(numTrainCorrect)/float(len(train_lt))
+	print "Training accuracy:"
+	print fracTrainCorrect
+	
+	trainLabels = BinaryLabels(train_lt)
+	evaluatorTrain = ROCEvaluation()
+	evaluatorTrain.evaluate(out1DecisionValues, trainLabels)
+	print "Training AUC:"
+	print evaluatorTrain.get_auROC()
+	
+	numValidCorrect = 0
+	numPosCorrect = 0
+	numNegCorrect = 0
+	numMajorityClassCorrect = 0
+	numMinorityClassCorrect = 0
+	for i in range(len(test_lt)):
+		# Iterate through validation labels and count the number that are the same as the predicted labels
+		if out2[i] == test_lt[i]:
+			# The current prediction is correct
+			numValidCorrect = numValidCorrect + 1
+			if (out2[i] == 1) and (test_lt[i] == 1):
+				# The prediction is a positive example
+				numPosCorrect = numPosCorrect + 1
+			else:
+				numNegCorrect = numNegCorrect + 1
+			if test_majorityClass[i] == 1:
+				numMajorityClassCorrect = numMajorityClassCorrect + 1
+			else:
+				numMinorityClassCorrect = numMinorityClassCorrect + 1
+	fracValidCorrect = float(numValidCorrect)/float(len(test_lt))
+	print "Validation accuracy:"
+	print fracValidCorrect
+	print "Fraction of correct positive examples:"
+	print float(numPosCorrect)/float(test_lt.count(1))
+	print "Fraction of correct negative examples:"
+	print float(numNegCorrect)/float(test_lt.count(0))
+	print "Fraction of correct majority class examples:"
+	print float(numMajorityClassCorrect)/float(test_majorityClass.count(1))
+	print "Fraction of correct minority class examples:"
+	print float(numMinorityClassCorrect)/float(test_majorityClass.count(0))
+	
+	validLabels = BinaryLabels(test_lt)
+	evaluatorValid = ROCEvaluation()
+	evaluatorValid.evaluate(out2DecisionValues, validLabels)
+	print "Validation AUC:"
+	print evaluatorValid.get_auROC()
+
+
 if __name__=='__main__':
 	print('LibSVM')
 	[train_xt, skippedLinesTrain] = makeStringList(TRAININGDATAFILENAME)
 	train_lt = makeIntList(TRAININGLABELSFILENAME, skippedLinesTrain)
 	[test_xt, skippedLinesValid] = makeStringList(VALIDATIONDATAFILENAME)
 	test_lt = makeIntList(VALIDATIONLABELSFILENAME, skippedLinesValid)
-	[out1, out2, out1DecisionValues, out2DecisionValues] = runShogunSVMDNASpectrumKernel(train_xt, train_lt, test_xt)
-	outputResultsClassification(out1, out2, out1DecisionValues, out2DecisionValues, train_lt, test_lt)
+	[out1, out2, out1DecisionValues, out2DecisionValues] = runShogunSVMDNAUlongSpectrumKernel(train_xt, train_lt, test_xt)
+	if len(sys.argv) > 10:
+		# There is majority class information
+		validationMajorityClassFileName = sys.argv[10]
+		test_majorityClass = makeIntList(validationMajorityClassFileName)
+		outputResultsClassificationWithMajorityClass(out1, out2, out1DecisionValues, out2DecisionValues, train_lt, test_lt, test_majorityClass)
+	else:
+		outputResultsClassification(out1, out2, out1DecisionValues, out2DecisionValues, train_lt, test_lt)
